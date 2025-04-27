f50180a805a71e278d848ee63cac85fe6854e55e
nyaruku <38096703+nyaruku@users.noreply.github.com>
2025-04-27 20:21:17 +0200

    changes

--- osu-tracker/test.cpp
+++ osu-tracker/test.cpp
@@ -5,11 +5,8 @@
 
 TEST_CASE("GET request to Google", "[network]") {
 
-    auto response = cpr::Get(cpr::Url{"https://api.kirino.sh/inspector/users/stats/13817114?skipDailyData=true&skipOsuData=true&skipExtras=true"});
-    std::cout << response.text << "\n";
-    std::cout << response.raw_header << "\n";
-    std::cout << response.reason << "\n";
-    std::cout << response.redirect_count << "\n";
+    auto response = cpr::Get(cpr::Url{"https://www.google.com"});
+
     // Check that the HTTP status code is 200 (OK)
     REQUIRE(response.status_code == 200);
 
