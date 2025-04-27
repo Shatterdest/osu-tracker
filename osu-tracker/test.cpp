e62f9a75be874f4cab7ed5e992ea5282f2f6e269
nyaruku <38096703+nyaruku@users.noreply.github.com>
2025-04-27 20:14:19 +0200

    - Fix windows ssl support

--- osu-tracker/test.cpp
+++ osu-tracker/test.cpp
@@ -1,11 +1,15 @@
 #define CATCH_CONFIG_MAIN
 #include "catch_amalgamated.hpp"
+#include <iostream> 
 #include <cpr/cpr.h>
 
 TEST_CASE("GET request to Google", "[network]") {
-    // Perform a GET request to Google's homepage
-    auto response = cpr::Get(cpr::Url{"https://www.google.com"});
 
+    auto response = cpr::Get(cpr::Url{"https://api.kirino.sh/inspector/users/stats/13817114?skipDailyData=true&skipOsuData=true&skipExtras=true"});
+    std::cout << response.text << "\n";
+    std::cout << response.raw_header << "\n";
+    std::cout << response.reason << "\n";
+    std::cout << response.redirect_count << "\n";
     // Check that the HTTP status code is 200 (OK)
     REQUIRE(response.status_code == 200);
 
