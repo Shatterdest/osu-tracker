ef79bb2a0b5d9e5f5ce7262801bb64b8876d6442
nyaruku <38096703+nyaruku@users.noreply.github.com>
2025-06-19 04:41:45 +0200

    more rewrite

--- osu-tracker/header/config.h
+++ osu-tracker/header/config.h
@@ -3,129 +3,117 @@
 #include <string>
 #include "console.h"
 
-// static data
-static std::string username;
+class config {
+public:
+	enum class gameMode {
+		osu = 0,
+		taiko = 1,
+		fruits = 2,
+		mania = 3
+	};
 
-std::vector<std::vector<std::string>> vec_application {
-	{"key","value"}, // 0
-	{"osu_id",""},// 1
-	{"client_id",""},// 2
-	{"client_secret",""},// 3
-	{"api_refreshInterval","7000"},// 4
-	{"gameMode","0"}, // 5: osu, taiko, fruits, mania
-	{"server","0"} // 6: [0: bancho], [1: titanic]
-};
+	enum class server {
+		bancho = 0,
+		titanic = 1
+	};
 
-enum data_type {
-	t_string = 0,
-	t_int = 1,
-	t_decimal  = 2
-};
+	struct application {
+		std::string username = "";
+		int osuId = 0;
+		int clientId = 0;
+		std::string clientSecret = "";
+		int apiInterval = 7000;
+		gameMode mode = gameMode::osu;
+		server server = server::bancho;
 
-enum format_type {
-	f_string = 0,
-	f_int = 1,
-	f_decimal = 2,
-	f_rank = 3,
-	f_time = 4,
-};
-
-struct data_entry {
-	std::string key;
-	std::string name;
-	std::string init;
-	std::string current;
-	std::string change;
-	data_type	type;
-	format_type format;
-	bool display;
-	bool bancho;
-	bool titanic;
-};
-
-std::vector<data_entry> vec_data{
-	{"level",		"Level",			"", "",	"", data_type::t_decimal,	format_type::f_decimal, true,	true,	true},	// 0
-	{"rankedScore",	"Ranked Score",		"", "",	"", data_type::t_int,		format_type::f_int,		true,	true,	true},	// 1
-	{"totalScore",	"Total Score",		"", "",	"", data_type::t_int,		format_type::f_int,		true,	true,	true},	// 2
-	{"ppRank",		"PP Rank",			"", "",	"", data_type::t_int,		format_type::f_rank,	true,	true,	true},	// 3
-	{"pp",			"PP",				"", "",	"", data_type::t_decimal,	format_type::f_decimal, true,	true,	true},	// 4
-	{"ppv1",		"PPv1",				"", "",	"", data_type::t_decimal,	format_type::f_decimal, true,	false,	true},	// 5
-	{"acc",			"Accuracy",			"", "",	"", data_type::t_decimal,	format_type::f_decimal, true,	true,	true},	// 6
-	{"playtime",	"Play Time",		"", "",	"", data_type::t_int,		format_type::f_time,	true,	true,	true},	// 7
-	{"playcount",	"Play Count",		"", "",	"", data_type::t_int,		format_type::f_int,		true,	true,	true},	// 8
-	{"totalHits",	"Total Hits",		"", "",	"", data_type::t_int,		format_type::f_int,		true,	true,	true},	// 9
-					    
-	{"silverSS",	"Rank SSH",			"", "",	"", data_type::t_int,		format_type::f_int,		true,	true,	true},	// 10
-	{"goldSS",		"Rank SS",			"", "",	"", data_type::t_int,		format_type::f_int,		true,	true,	true},	// 11
-	{"silverS",		"Rank SH",			"", "",	"", data_type::t_int,		format_type::f_int,		true,	true,	true},	// 12
-	{"goldS",		"Rank S",			"", "",	"", data_type::t_int,		format_type::f_int,		true,	true,	true},	// 13
-	{"a",			"Rank A",			"", "",	"", data_type::t_int,		format_type::f_int,		true,	true,	true},	// 14
-	{"b",			"Rank B",			"", "",	"", data_type::t_int,		format_type::f_int,		true,	true,	true},	// 15
-	{"c",			"Rank C",			"", "",	"", data_type::t_int,		format_type::f_int,		true,	true,	true},	// 16
-	{"d",			"Rank D",			"", "",	"", data_type::t_int,		format_type::f_int,		true,	true,	true},	// 17
-	{"totalSS",		"Total SS",			"", "",	"", data_type::t_int,		format_type::f_int,		true,	true,	true},	// 18
-	{"totalS",		"Total S",			"", "",	"", data_type::t_int,		format_type::f_int,		true,	true,	true},	// 19
-					    
-	{"clears",		"Profile Clears",	"", "",	"", data_type::t_int,		format_type::f_int,		true,	true,	true},	// 20
-	{"totalClears",	"Total Clears",		"", "",	"", data_type::t_int,		format_type::f_int,		true,	true,	true},	// 21
-	{"completion",	"Completion%",		"", "",	"", data_type::t_decimal,	format_type::f_decimal, true,	true,	true},	// 22
-	{"scoreRank",	"Score Rank",		"", "",	"", data_type::t_int,		format_type::f_rank,	true,	true,	true},	// 23
-					    
-	{"targetRank",	"Target Rank",		"", "",	"", data_type::t_int,		format_type::f_rank,	true,	true,	true},	// 24
-	{"targetUser",	"Target Player",	"", "",	"", data_type::t_string,	format_type::f_string,	true,	true,	true},	// 25
-	{"targetScore",	"Target Score",		"", "",	"", data_type::t_int,		format_type::f_int,		true,	true,	true},	// 26
-
-};
-
-/*
-*	1: vector that has string vectors
-*	2: key string
-*	3: field string
-*	4: value to set
-*/
-int setConfig(std::vector<std::vector<std::string>>& vec, std::string key, std::string field, std::string value) {
-	int keyIndex = -1;
-	int fieldIndex = -1;
-
-	for (size_t i = 0; i < vec[0].size(); i++) {
-		if (vec[0][i] == field) {
-			fieldIndex = i;
-			break;
+		std::vector<std::tuple<std::string, std::string>> toArray() const {
+			return {
+				{"username", username}
+				,{"osuId", std::to_string(osuId)}
+				,{"clientId", std::to_string(clientId)}
+				,{"clientSecret", clientSecret}
+				,{"apiInterval", std::to_string(apiInterval)}
+				,{"mode", std::to_string(static_cast<int>(mode))}
+				,{"server", std::to_string(static_cast<int>(server))}
+			};
 		}
-	}
-	for (size_t i = 1; i < vec.size(); i++) {
-		if (vec[i][0] == key) {
-			keyIndex = i;
-			break;
-		}
-	}
+	};
 
-	if (keyIndex == -1 || fieldIndex == -1) {
-		return -1;
-	}
-	else {
-		vec[keyIndex][fieldIndex] = value;
-		return 0;
-	}
-}
+	enum class dataType {
+		t_string = 0,
+		t_int = 1,
+		t_decimal = 2
+	};
 
-/*
-*	1: vector that has string vectors
-*	2: key string
-*	3: field string
-*/
-std::string getConfig(std::vector<std::vector<std::string>>& vec, std::string key, std::string field) {
-	for (size_t i = 1; i < vec.size(); i++) {
-		if (vec[i][0] == key) {
-			for (size_t j = 0; j < vec[i].size(); j++) {	
-				if (vec[0][j] == field) {
-					return vec[i][j];
-				}
-			}
+	enum class formatType {
+		f_string = 0,
+		f_int = 1,
+		f_decimal = 2,
+		f_rank = 3,
+		f_time = 4,
+	};
+
+	struct dataEntry {
+		std::string key;
+		std::string name;
+		std::string init;
+		std::string current;
+		std::string change;
+		dataType dataType;
+		formatType formatType;
+		bool display;
+		bool banchoSupport;
+		bool titanicSupport;
+
+		std::vector<std::tuple<std::string, std::string>> toArray() const {
+			return {
+				{"key", key}
+				,{"name", name}
+				,{"init", init}
+				,{"current", current}
+				,{"change", change}
+				,{"dataType", std::to_string(static_cast<int>(dataType))}
+				,{"formatType", std::to_string(static_cast<int>(formatType))}
+				,{"display", boolToString(display)}
+				,{"banchoSupport", boolToString(banchoSupport)}
+				,{"titanicSupport", boolToString(titanicSupport)}
+			};
 		}
-	}
-	return "";
-}
+	};
+
+	std::vector<dataEntry> vecData {
+		{"level",		"Level",			"", "",	"", dataType::t_decimal,	formatType::f_decimal,	true,	true,	true}
+		,{"rankedScore","Ranked Score",		"", "",	"", dataType::t_int,		formatType::f_int,		true,	true,	true}
+		,{"totalScore",	"Total Score",		"", "",	"", dataType::t_int,		formatType::f_int,		true,	true,	true}
+		,{"ppRank",		"PP Rank",			"", "",	"", dataType::t_int,		formatType::f_rank,		true,	true,	true}
+		,{"pp",			"PP",				"", "",	"", dataType::t_decimal,	formatType::f_decimal,	true,	true,	true}
+		,{"ppv1",		"PPv1",				"", "",	"", dataType::t_decimal,	formatType::f_decimal,	true,	false,	true}
+		,{"acc",		"Accuracy",			"", "",	"", dataType::t_decimal,	formatType::f_decimal,	true,	true,	true}
+		,{"playtime",	"Play Time",		"", "",	"", dataType::t_int,		formatType::f_time,		true,	true,	true}
+		,{"playcount",	"Play Count",		"", "",	"", dataType::t_int,		formatType::f_int,		true,	true,	true}
+		,{"totalHits",	"Total Hits",		"", "",	"", dataType::t_int,		formatType::f_int,		true,	true,	true}
+
+		,{"silverSS",	"Rank SSH",			"", "",	"", dataType::t_int,		formatType::f_int,		true,	true,	true}
+		,{"goldSS",		"Rank SS",			"", "",	"", dataType::t_int,		formatType::f_int,		true,	true,	true}
+		,{"silverS",	"Rank SH",			"", "",	"", dataType::t_int,		formatType::f_int,		true,	true,	true}
+		,{"goldS",		"Rank S",			"", "",	"", dataType::t_int,		formatType::f_int,		true,	true,	true}
+		,{"a",			"Rank A",			"", "",	"", dataType::t_int,		formatType::f_int,		true,	true,	true}
+		,{"b",			"Rank B",			"", "",	"", dataType::t_int,		formatType::f_int,		true,	true,	true}
+		,{"c",			"Rank C",			"", "",	"", dataType::t_int,		formatType::f_int,		true,	true,	true}
+		,{"d",			"Rank D",			"", "",	"", dataType::t_int,		formatType::f_int,		true,	true,	true}
+		,{"totalSS",	"Total SS",			"", "",	"", dataType::t_int,		formatType::f_int,		true,	true,	true}
+		,{"totalS",		"Total S",			"", "",	"", dataType::t_int,		formatType::f_int,		true,	true,	true}
+
+		,{"clears",		"Profile Clears",	"", "",	"", dataType::t_int,		formatType::f_int,		true,	true,	true}
+		,{"totalClears","Total Clears",		"", "",	"", dataType::t_int,		formatType::f_int,		true,	true,	true}
+		,{"completion",	"Completion%",		"", "",	"", dataType::t_decimal,	formatType::f_decimal,	true,	true,	true}
+		,{"scoreRank",	"Score Rank",		"", "",	"", dataType::t_int,		formatType::f_rank,		true,	true,	true}
+
+		,{"targetRank",	"Target Rank",		"", "",	"", dataType::t_int,		formatType::f_rank,		true,	true,	true}
+		,{"targetUser",	"Target Player",	"", "",	"", dataType::t_string,		formatType::f_string,	true,	true,	true}
+		,{"targetScore","Target Score",		"", "",	"", dataType::t_int,		formatType::f_int,		true,	true,	true}
+	};
+};
 
 // write config
 void writeConfig() {
@@ -134,15 +122,12 @@ void writeConfig() {
 	input += "//PLEASE DONT MANUALLY EDIT THIS FILE\n"
 			 "//(you still can,but you might break it)\n"
 			 "//(If you want to reset all settings, just delete this file)\n\n";
-	input += "[ApplicationConfig]\n";
-	for (int i = 1; i < vec_application.size(); i++) {
-		// 0 = key : 0
-		// 1 = value : 1
-		input += vec_application[i][0] + ";" + vec_application[i][1] + "\n";
+	input += "[ApplicationConfig]";
+	for (const auto& [key, value] : config::application{}.toArray()) {
+		input += "\n" + key + ";" + value;
 	}
-	input += "\n";
-	input += "[TrackerConfig]\n";
-	for (int i = 1; i < vec_tracker.size(); i++) {
+	input += "\n\n[TrackerConfig]";
+	for (const auto& _vecData : config::vecData.size()) {
 		// 0 = key : 0
 		// 2 = display : 1
 		input += vec_tracker[i][0] + ";" + vec_tracker[i][2] + "\n";
